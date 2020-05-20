b575807e9cfa76020aa45c24974a1cf493bfc910
andres-torres-2020 <andres-torres-192020@gmail.com>
2020-05-20 10:23:51 -0400

    Initial version.

---
+++ CoolMVCStuff/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoolMVCStuff.Models;
+
+namespace CoolMVCStuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult AlphabetForm()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Result(char Letter, int Position, string Origin)
+        {
+            ViewBag.Letter = Letter;
+            ViewBag.p = Position;
+            ViewBag.O = Origin;
+            return View();
+        }
+        public IActionResult Result2(Alphabet alphabet)
+        {
+            return View(alphabet);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoolMVCStuff/Models/Alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolMVCStuff.Models
+{
+    public class Alphabet
+    {
+        public char Letter { get; set; }
+        public int Position { get; set; }
+        public string Origin { get; set; }
+    }
+}
